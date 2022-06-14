6b9eb3a4ec9629a391f7463f2bb954d652dcd41f
Hammerill <www.hammerill@gmail.com>
2022-06-14 20:57:51 +0300

    Make game buildable

--- uNext/FireBall.cpp
+++ uNext/FireBall.cpp
@@ -2,6 +2,8 @@
 #include "Core.h"
 #include <math.h>
 
+#define M_PI 3.14159265358979323846
+
 /* ******************************************** */
 
 FireBall::FireBall(int iXPos, int iYPos, int radius, int nSliceID, bool moveDirection) {
